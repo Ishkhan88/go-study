6855a79817ffacfadab78faa3632a5026b860d76
Ishkhan88 <Khalatian88@yandex.ru>
2025-11-12 10:36:06 +0300

    Change structure

---
+++ internal/model/user.go
@@ -0,0 +1,16 @@
+package model
+
+import "time"
+
+// User - содержит основные данные профиля.
+
+type User struct {
+	ID        int       `json:"id"`         // уникальный идентификатор пользователя
+	FirstName string    `json:"first_name"` // имя
+	LastName  string    `json:"last_name"`  // фамилия
+	Email     string    `json:"email"`      // адрес электронной почты
+	Phone     string    `json:"phone"`      // номер телефона
+	AvatarURL string    `json:"avatar_url"` // ссылка на аватар
+	CreatedAt time.Time `json:"created_at"` // когда создан профиль
+	UpdatedAt time.Time `json:"updated_at"` // когда обновлён профиль
+}
